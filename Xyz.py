c59c789db4af5f1f690a4a492305b3a13b9b94a6
𝐌𝐀𝐅𝐈𝐘𝐀\٠\l❼⑧❻ ‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎ <younisxyz18@gmail.com>
2026-01-02 15:52:37 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,24 +1,24 @@
 """
     @ code by ---( Younis john )---
     @ Github : https://github.com/YounisXyz
-    @ WhatsApp : https://wa.me/923194999455
+    @ WhatsApp : https://wa.me/923494445455
     
 """
 import os, sys, platform
  
-os.system('rm -rf Safeum.so')
+os.system('rm -rf SafeUm.so')
  
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf Safeum.so')
+        os.system('rm -rf SafeUM.so')
 except:
     pass
  
  
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('Safeum.so'):
-        os.system('curl -L https://github.com/YounisXyz/executables/blob/main/Safeum.cpython-312.so?raw=true -o Safeum.so') 
-        import Safeum
+    if not os.path.isfile('SafeUM.so'):
+        os.system('curl -L https://github.com/YounisXyz/executables/blob/main/SafeUM.cpython-312.so?raw=true -o SafeUM.so') 
+        import SafeUM
     else:
-        import Safeum
+        import SafeUM
